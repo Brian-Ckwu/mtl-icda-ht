e7779a28c629d51bc195fc9bdd29474b8ade241a
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-10 22:02:57 +0800

    modify utils.py: adjust functions for cleaner codes

--- components_testing/diagnosis/utils.py
+++ components_testing/diagnosis/utils.py
@@ -1,22 +1,32 @@
+import random
+import numpy as np
 import torch
 from tqdm import tqdm
+
+def same_seeds(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  
+    np.random.seed(seed)  
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 def trainer(train_loader, val_loader, model, criterion, config, device):
     optimizer = getattr(torch.optim, config["optimizer"])(model.parameters(), **config["optimizer_hparams"])
 
-    # calculate initial val loss
-    val_loss = evaluate_model_loss(val_loader, model, criterion, device)
-    print(f"Initial val loss: {val_loss:.4f}")
-
     # some variables
-    loss_record = {"train": list(), "val": list()}
-    min_loss = float("inf")
+    record = {
+        "train": {"acc": list(), "loss": list()},
+        "val": {"acc": list(), "loss": list()}
+    }
+    best_val_acc = 0
     step = 0
 
     for epoch in range(1, config["n_epochs"] + 1):
-        # train
-        model.train()
         for x, y in train_loader:
+            model.train()
             # move data to device
             move_bert_input_to_device(x, device)
             y = y.to(device)
@@ -29,22 +39,47 @@ def trainer(train_loader, val_loader, model, criterion, config, device):
             loss.backward()
             optimizer.step()
             
-            # record loss
-            loss_record["train"].append(loss.detach().cpu().item())
-            if step % 100 == 0:
-                print(f"Step {step}: batch_train_loss = {loss_record['train'][-1]}")
+            # evaluate model every k steps
+            if step % config["checkpoint_steps"] == 0:
+                print("Evaluating model at step {}...".format(step))
+                best_val_acc = update_evaluation(val_loader, model, criterion, config, device, record, best_val_acc)
             step += 1
         
-        # evaluate
-        val_loss = evaluate_model_loss(val_loader, model, criterion, device)
-        loss_record["val"].append(val_loss)
-        if val_loss < min_loss:
-            min_loss = val_loss
-            torch.save(model.state_dict(), config["model_save_path"])
-            print(f"Best model saved (epoch = {epoch:2d}, loss = {min_loss:.4f})")
+        # evaluate model every epoch
+        print("===== Evaluting model at epoch {}... =====".format(epoch))
+        best_val_acc = update_evaluation(val_loader, model, criterion, config, device, record, best_val_acc)
     
-    return min_loss, loss_record
+    record["best_val_acc"] = best_val_acc
+    return record
 
+def update_evaluation(data_loader, model, criterion, config, device, record, best_acc):
+    acc = evaluate_model_acc(data_loader, model, device)
+    loss = evaluate_model_loss(data_loader, model, criterion, device)
+    record["val"]["acc"].append(acc)
+    record["val"]["loss"].append(loss)
+    print(f"Acc: {acc:.4f} / Loss: {loss:.4f}")
+    if acc > best_acc:
+        best_acc = acc
+        torch.save(model.state_dict(), "./models/{}.pth".format(config["model_save_name"]))
+        print("Best model saved at this checkpoint.")
+
+    return best_acc
+
+def evaluate_model_acc(data_loader, model, device):
+    model.eval()
+    n_correct = 0
+    n_predict = 0
+
+    for x, y in data_loader:
+        x = move_bert_input_to_device(x, device)
+        y = y.to(device)
+        with torch.no_grad():
+            pred = model(x)
+            n_correct += (pred.argmax(dim=-1) == y).sum().cpu().detach().item()
+            n_predict += len(y)
+    
+    acc = n_correct / n_predict
+    return acc
 
 def evaluate_model_loss(data_loader, model, criterion, device):
     model.eval()
@@ -57,7 +92,7 @@ def evaluate_model_loss(data_loader, model, criterion, device):
         with torch.no_grad():
             pred = model(x)
             loss = criterion(pred, y)
-        total_val_loss += loss.detach().cpu().item() * len(x["input_ids"])    
+        total_val_loss += loss.detach().cpu().item() * len(y)
     
     mean_val_loss = total_val_loss / len(data_loader.dataset)
     return mean_val_loss
