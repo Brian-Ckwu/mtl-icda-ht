28aed64b14825eeafd01667f88c5736adc8c5137
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-10 17:55:01 +0800

    modify data.py: implement class MedicalDiagnosisDataset

--- components_testing/diagnosis/data.py
+++ components_testing/diagnosis/data.py
@@ -2,32 +2,29 @@ import json
 from multiprocessing.sharedctypes import Value
 import torch
 from torch.utils.data import Dataset
-from transformers import BertTokenizer, DataCollatorWithPadding
+from transformers import BertTokenizerFast
 import pandas as pd
 import numpy as np
 
-class EMRDataset(Dataset):
-    def __init__(self, emr_file, mode="train"):
-        df = pd.read_csv(emr_file).dropna()
-        data = get_text_label_pairs(df) # --> ndarray: (text, label)
-        data = even_split_by_labels(data, mode) # --> train or val data
-        self.x = data[:, 0]
-        self.y = data[:, 1]
+class MedicalDiagnosisDataset(Dataset):
+    def __init__(self, emrs: list[str], dx_labels: list[int], tokenizer: BertTokenizerFast):
+        self.x = emrs
+        self.y = dx_labels
+        assert len(self.x) == len(self.y)
+        self.tokenizer=tokenizer
     
     def __len__(self):
         return len(self.x)
     
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
-
-# load utility files
-with open("./icd2idx.json") as f:
-    icd2idx = json.load(f)
-
-def get_text_label_pairs(df):
-    pairs = df[["PRESENTILLNESS", "In_ICD"]]
-    pairs.loc[:, "In_ICD"] = pairs.loc[:, "In_ICD"].apply(lambda icd: icd2idx[icd])
-    return pairs.values
+        # find the indexed data
+        emr = self.x[idx]
+        dx = self.y[idx]
+        # transform
+        x = self.tokenizer(emr, truncation=True, verbose=False).convert_to_tensors(tensor_type="pt", prepend_batch_axis=False)
+        # target transform
+        y = torch.LongTensor(dx)        
+        return x, y
 
 def even_split_by_labels(data, mode):
     data_l = list()
@@ -41,14 +38,3 @@ def even_split_by_labels(data, mode):
         else:
             raise ValueError("mode should be either train or val")
     return np.array(data_l)
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-def bert_batch_collator(batch):
-    texts, labels = list(), list()
-    for text, label in batch:
-        texts.append(text)
-        labels.append(label)
-    features = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-    labels = torch.tensor(labels)
-    return features, labels
