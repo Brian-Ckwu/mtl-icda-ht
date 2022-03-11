19f12b745b91161b7660efa472ec83ee8af0885b
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-11 21:39:46 +0800

    modify data.py: add convert_icds_to_indices() & split_by_div()

--- multitask_models/diagnosis_ner/data.py
+++ multitask_models/diagnosis_ner/data.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Iterable
 from torch.utils.data import Dataset
 from transformers import BertTokenizerFast
 
@@ -14,7 +15,7 @@ class MTLDiagnosisNERDataset(Dataset):
         self.x = emrs
         self.y_dx = dx_labels
         self.y_ner = ner_labels
-        assert len(self.x) == len(self.y_dx) == self.y_ner # check equal length
+        assert len(self.x) == len(self.y_dx) == len(self.y_ner) # check equal length
         # tokenizer
         self.tokenizer = tokenizer
         self.ignore_index = ignore_index
@@ -31,12 +32,47 @@ class MTLDiagnosisNERDataset(Dataset):
         encoded_emr = self.tokenizer(emr, truncation=True, return_offsets_mapping=True, verbose=False).convert_to_tensors(tensor_type="pt", prepend_batch_axis=False)
         offset_mapping = encoded_emr.pop("offset_mapping")
         # target transform - dx
-        y_dx = torch.LongTensor(dx)
+        y_dx = torch.LongTensor([dx])
         # target transform - ner
         y_ner = torch.LongTensor(bert_tokens_to_ner_labels(offset_mapping, target_indices=ner_labels, ignore_index=self.ignore_index))
 
         return encoded_emr, y_dx, y_ner
 
+def convert_icds_to_indices(icds: list[str], full_code: bool = True) -> list[int]:
+    # utility functions
+    def get_converted_icd(icd):
+        return str(icd) if full_code else str(icd)[:3]
+
+    def get_icd_idx_mapping():
+        icd2idx = dict()
+        for icd in icds:
+            icd = get_converted_icd(icd)
+            if icd not in icd2idx:
+                icd2idx[icd] = len(icd2idx)
+        return icd2idx
+    
+    # conversion
+    icd2idx = get_icd_idx_mapping()
+    indices = list()
+    for icd in icds:
+        icd = get_converted_icd(icd)
+        idx = icd2idx[icd]
+        indices.append(idx)
+
+    return indices
+
+def split_by_div(data: Iterable, fold: int, mode: str) -> list:
+    data_l = list()
+    for i, item in enumerate(data):
+        if mode == "train":
+            if i % fold != 0:
+                data_l.append(item)
+        elif mode == "val":
+            if i % fold == 0:
+                data_l.append(item)
+        else:
+            raise ValueError("mode should be either train or val")
+    return data_l
 
 def bert_tokens_to_ner_labels(offset_mapping, target_indices: set[int], ignore_index: int = -100) -> list[int]:
     offsets = offset_mapping
