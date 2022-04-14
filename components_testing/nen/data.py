918d40f3b4e25a5c74a58d7d06cca918ad496e28
Brian-Ckwu <sonny40920710@gmail.com>
2022-04-14 20:38:55 +0800

    complete first version of BertNENDataset

--- components_testing/nen/data.py
+++ components_testing/nen/data.py
@@ -1,32 +1,58 @@
-import torch
+import random
 from typing import List, Tuple, Dict
+
+import torch
 from torch.utils.data import Dataset
 from transformers import BertTokenizerFast
 
 class BertNENDataset(Dataset):
-    def __init__(self, emrs: List[str], ner_spans_l: List[List[Tuple[int]]], mention2cui: Dict[str, str], tokenizer: BertTokenizerFast):
+    def __init__(self, emrs: List[str], ner_spans_l: List[List[Tuple[int]]], mention2cui: Dict[str, str], cui2name: Dict[str, str], tokenizer: BertTokenizerFast):
         assert len(emrs) == len(ner_spans_l)
         # attributes
         self.emrs = emrs
         self.ner_spans_l = ner_spans_l
         self.mention2cui = mention2cui
+        self.cui2name = cui2name
+        self.cuis = list(cui2name.keys())
         self.tokenizer = tokenizer
     
     def __len__(self):
         return len(self.emrs)
     
-    def __getitem__(self, idx): # -> BE (EMR), token_indices_l (NER), entities (NEN)
+    def __getitem__(self, idx): # -> BE (EMR), token_indices_l (NER), CUIs (NEN)
         emr = self.emrs[idx]
         ner_spans = self.ner_spans_l[idx]
-        # TODO: filter ner_spans according to mention2entity & construct entity labels
+        # Filter ner_spans according to mention2cui & construct entity labels
+        mapped_ner_spans = list()
+        cuis = list()
+        for ner_span in ner_spans:
+            start, end = ner_span
+            emr_span = emr[start:end].lower().strip()
+            if emr_span in self.mention2cui:
+                mapped_ner_spans.append(ner_span)
+                cuis.append(self.mention2cui[emr_span])
 
         be = self.tokenizer(emr, return_offsets_mapping=True)
         offsets = be.pop("offset_mapping")
-
-        token_indices_l = self.spans_to_token_indices_l(ner_spans, offsets)
+        token_indices_l = self.spans_to_token_indices_l(mapped_ner_spans, offsets)
         
         be = be.convert_to_tensors("pt", prepend_batch_axis=True)
-        return be, token_indices_l
+        # Align token_indices_l & CUIs (remove empty list)
+        assert len(token_indices_l) == len(cuis)
+        token_indices_l_clean, cuis_clean = list(), list()
+        for token_indices, cui in zip(token_indices_l, cuis):
+            if token_indices != []:
+                token_indices_l_clean.append(token_indices)
+                cuis_clean.append(cui)
+        return be, token_indices_l_clean, cuis_clean
+    
+    def random_negative_sampling(self, target_cui: str, batch_size: int):
+        random_cuis = random.sample(self.cuis, k=batch_size)
+        negatives = list()
+        for random_cui in random_cuis:
+            if random_cui != target_cui:
+                negatives.append(random_cui)
+        return negatives[:batch_size - 1] # return 'batch_size - 1' (e.g. 15) negative samples
     
     @staticmethod
     def spans_to_token_indices_l(spans: List[Tuple[int]], offsets: List[Tuple[int]]) -> List[List[int]]:
@@ -49,3 +75,4 @@ class BertNENDataset(Dataset):
             token_indices_l.append(token_indices)
         
         return token_indices_l
+    
